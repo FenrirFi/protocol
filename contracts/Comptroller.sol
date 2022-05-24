0445868fcc930427f687b87c5af1c78878669f63
0xLoki <fenrirfinance123@gmail.com>
2022-05-24 12:28:59 +0530

    Deployed new comptroller on kcc with rewards transfer disabled

--- contracts/Comptroller.sol
+++ contracts/Comptroller.sol
@@ -1388,12 +1388,12 @@ contract Comptroller is ComptrollerV7Storage, ComptrollerInterface, ComptrollerE
      * @return The amount of COMP which was NOT transferred to the user
      */
     function grantCompInternal(address user, uint amount) internal returns (uint) {
-        Comp comp = Comp(getCompAddress());
-        uint compRemaining = comp.balanceOf(address(this));
-        if (amount > 0 && amount <= compRemaining) {
-            comp.transfer(user, amount);
-            return 0;
-        }
+        // Comp comp = Comp(getCompAddress());
+        // uint compRemaining = comp.balanceOf(address(this));
+        // if (amount > 0 && amount <= compRemaining) {
+        //     comp.transfer(user, amount);
+        //     return 0;
+        // }
         return amount;
     }
 
